925a7c31b5b6bd63f9f18a74afea26c97bce8c80
Kevin "qwazerty" Houdebert <houdeb_k@yaka.epita.fr>
2014-06-12 02:38:05 +0200

    Add build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
 usage() {
-    echo "Usage: $0 <path_to_dockerfile>"
+    echo "Usage: $0 [-a|--all] [Dockerfile] ..."
 }
+
+build() {
+    DOCKER_PATH=$(echo $1 | sed 's/.*build//' | cut -d/ -f2,3 | sed 's|/|-|')
+    docker build --rm -t $DOCKER_PATH $(dirname $1)
+}
+
+full_build() {
+    for i in $(find . -name Dockerfile); do
+        build $i
+    done
+}
+
+cd $(dirname $0)
 
 if [ $# -ne 1 ]; then
     usage
     exit 1
 fi
 
-docker build -t $(cut -d/ -f2,3 <<< $1 | sed 's#/#-#') $(cut -d/ -f-3 <<< $1)
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -a|--all)
+            full_build
+            exit 0
+            ;;
+        *)
+            build $1
+            shift
+            ;;
+    esac
+done
