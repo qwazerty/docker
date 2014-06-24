48de1918f9a5bb6ed1df573d7c25c5f7ce06381e
Kevin Houdebert <kevin.houdebert@gmail.com>
2014-06-24 14:33:16 +0200

    Rename build script. Add no cache option

---
+++ docker-build.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+NO_CACHE=
+
+usage() {
+    echo "Usage:"
+    echo -e "\t$0 [-a|--all]"
+    echo -e "\t$0 [-c|--no-cache] Dockerfile..."
+    echo "Example:"
+    echo -e "\t$0 -c ./build/wheezy/nginx/Dockerfile"
+}
+
+build() {
+    DOCKER_PATH=$(echo $1 | sed 's/.*build//' | cut -d/ -f2,3 | sed 's|/|-|')
+    docker build $NO_CACHE --rm -t $DOCKER_PATH $(dirname $1)
+}
+
+# FIXME: Handle dependencies
+full_build() {
+    for i in $(find . -name Dockerfile); do
+        build $i
+    done
+}
+
+cd $(dirname $0)
+
+if [ $# -lt 1 ]; then
+    usage
+    exit 1
+fi
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -c|--no-cache)
+            NO_CACHE="--no-cache"
+            shift
+            ;;
+        -a|--all)
+            full_build
+            exit 0
+            ;;
+        *)
+            build $1
+            shift
+            ;;
+    esac
+done
